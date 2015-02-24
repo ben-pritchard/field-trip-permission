5e5e82102506cef9594f189be6185ff7e161832d
Ben Pritchard and Tucker Mayo <ben.s.pritchard@gmail.com>
2015-02-24 10:07:46 -0800

    refactor controller logic to include factory.

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,6 +1,5 @@
 studentRoster.controller("StudentsCtrl", function StudentsCtrl($scope, StudentsFactory) {
   $scope.students = StudentFactory.students;
   $scope.StudentsFactory = StudentsFactory;
-
   };
 });
